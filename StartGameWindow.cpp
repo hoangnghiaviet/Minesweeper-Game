3e8328924af94bfbdd48c797ae5dfc169c8263cc
Hoang Nghia Viet <viethoangnghia@gmail.com>
2021-11-24 10:49:50 +0700

    Update StartGameWindow Library

--- StartGameWindow.cpp
+++ StartGameWindow.cpp
@@ -7,9 +7,16 @@ using namespace std;
 using namespace sf;
 
 #include "StartGameWindow.h"
-#include "InGameWindow.h"
 #include "ButtonClass.h"
 #include "MouseEvent.h"
+#include "StartGameMenu.h"
+
+// There are 5 buttons:
+// start_game_button[0]: Load game button (move to LoadGameWindow)
+// start_game_button[1]: New game button (move to NewGameWindow)
+// start_game_button[2]: Leader board button (move to LeaderBoardWindow)
+// start_game_button[3]: Instruction button (move to InstructionWindow)
+// start_game_button[4]: Author button (move to AuthorWindow)
 
 void StartGameWindow()
 {
@@ -41,11 +48,11 @@ void StartGameWindow()
     }
 
     // button constructor
-    ButtonClass B[5];
+    ButtonClass start_game_button[5];
     for(int i = 0; i < 5; ++i)
     {
-        B[i] = ButtonClass(Vector2f(200.f, 30.f), Vector2f(200, 270 + i * 50));
-        B[i].button.setTexture(&button_texture[i]);
+        start_game_button[i] = ButtonClass(Vector2f(200.f, 30.f), Vector2f(200, 270 + i * 50));
+        start_game_button[i].button.setTexture(&button_texture[i]);
     }
 
     // display window
@@ -64,22 +71,23 @@ void StartGameWindow()
         Vector2i mouse_position = Mouse::getPosition(start_game_window);
         for(int i = 0; i < 5; ++i)
         {
-            if(check_mouse_clicked(mouse_position, B[i].button.getPosition(), B[i].button.getSize()))
+            if(check_mouse_clicked(mouse_position, start_game_button[i].button.getPosition(), start_game_button[i].button.getSize()))
             {
                 if(click == "left")
                 {
                     start_game_window.close();
+                    move_to_start_game_menu(i);
                 }
-                B[i].button.setFillColor(Color::Red);
+                start_game_button[i].button.setFillColor(Color::Red);
             }
-            else B[i].button.setFillColor(Color::White);
+            else start_game_button[i].button.setFillColor(Color::White);
         }
 
         start_game_window.clear(Color::White);
         start_game_window.draw(start_game_background);
         for(int i = 0; i < 5; ++i)
         {
-            start_game_window.draw(B[i].button);
+            start_game_window.draw(start_game_button[i].button);
         }
         start_game_window.display();
     }
