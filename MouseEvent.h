669853d6fedd1cfb94a4b4d4ad044e6230543c45
Hoang Nghia Viet <viethoangnghia@gmail.com>
2021-11-25 20:59:18 +0700

    Update MouseEvent Library

--- MouseEvent.h
+++ MouseEvent.h
@@ -3,6 +3,10 @@
 using namespace std;
 using namespace sf;
 
+#include "ButtonClass.h"
+
 bool check_mouse_clicked(Vector2i mouse_position, Vector2f object_position, Vector2f object_size);
 
 string get_mouse_clicked();
+
+int get_button_clicked(ButtonClass button[], int total_button, RenderWindow &window);
